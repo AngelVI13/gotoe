ee443dbbc76ce93455a0827f61cd7409eb8500b1
Angel ILIEV <a.v.iliev13@gmail.com>
2019-07-30 14:10:34 +0300

    Added timing information

--- main.go
+++ main.go
@@ -1,10 +1,15 @@
 package main
 
 import "fmt"
+import "time"
+
 import board "local/gotoe/board"
 import uct "local/gotoe/uct"
 
 func main() {
 	b := board.CreateNewBoard()
+
+	start := time.Now()
 	fmt.Println(uct.GetEngineMove(&b, 10000))
+	fmt.Println(time.Since(start))
 }
